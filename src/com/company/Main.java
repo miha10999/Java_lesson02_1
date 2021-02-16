400ed1f1d5aa5d29f8e07d5a777340ec20907bc8
miha10999 <mihey10999@gmail.com>
2021-02-16 23:41:55 +0300

    правка №1

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+
+public class Main {
+
+/*
+ * @author khlyustov mikhail
+ * Задача №1 - Первая программа
+ ** Метод выводит на консоль текст “Hello World!"
+  * @since  JDK8.0
+  */
+
+    public static void main(String[] args) {
+        System.out.println("“Hello World!");
+    }
+}
+
